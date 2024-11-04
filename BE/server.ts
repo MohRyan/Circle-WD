bcd122fd200d15e39a8fe93e7ca637b62d0ca3f9
MohRyan <ryanmoh735@gmail.com>
2024-11-04 21:34:48 +0700

    fix Bug Profile

--- BE/server.ts
+++ BE/server.ts
@@ -9,10 +9,10 @@ dotenv.config();
 const PORT = 4000;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use("/uploads", express.static(path.join(__dirname, "src/uploads"))) // untuk menjadikan sebuah folder bisa diakses secara global
-app.use(cors());
 
 app.use(indexRouter);
 
